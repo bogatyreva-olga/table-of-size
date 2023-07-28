e58f9438fe82e90507ed30ffb724ae5ada986fe7
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-29 00:01:21 +0300

    add data with object

---
+++ data.js
@@ -0,0 +1,369 @@
+let tableTernua = {
+    "data": [
+        [
+            "Ternua — Женская одежда",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ],
+        [
+            "Размер",
+            "XS",
+            "S",
+            "M",
+            "L",
+            "XL",
+            "XXL"
+        ],
+        [
+            "Обхват груди, см",
+            "80-84",
+            "85-89",
+            "90-94",
+            "95-99",
+            "100-104",
+            "105-110"
+        ],
+        [
+            "Обхват талии, см",
+            "61-65",
+            "66-70",
+            "71-75",
+            "76-80",
+            "81-85",
+            "86-90"
+        ],
+        [
+            "Обхват бедер, см",
+            "86-90",
+            "91-95",
+            "96-100",
+            "101-105",
+            "106-110",
+            "111-115"
+        ],
+        [
+            "Длина рукава, см ",
+            "70-71",
+            "72-73",
+            "74-75",
+            "76-77",
+            "78-79",
+            "80-81"
+        ],
+        [
+            "Длина брюк (по внешнему шву)",
+            "92-95",
+            "96-99",
+            "100-103",
+            "104-107",
+            "108-110",
+            "111-113"
+        ]
+    ],
+}
+
+let otherInfo = {
+    "merge": [],
+    "cell": [
+        {
+            "row": 0,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 0,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 0,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 0,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 0,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 0,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 0,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 1,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 1,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 1,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 1,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 1,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 1,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 1,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 2,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 2,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 2,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 2,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 2,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 2,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 2,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 3,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 3,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 3,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 3,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 3,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 3,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 3,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 4,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 4,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 4,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 4,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 4,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 4,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 4,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 5,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 5,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 5,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 5,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 5,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 5,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 5,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        },
+        {
+            "row": 6,
+            "col": 0,
+            "prop": 0,
+            "visualCol": 0
+        },
+        {
+            "row": 6,
+            "col": 1,
+            "prop": 1,
+            "visualCol": 1
+        },
+        {
+            "row": 6,
+            "col": 2,
+            "prop": 2,
+            "visualCol": 2
+        },
+        {
+            "row": 6,
+            "col": 3,
+            "prop": 3,
+            "visualCol": 3
+        },
+        {
+            "row": 6,
+            "col": 4,
+            "prop": 4,
+            "visualCol": 4
+        },
+        {
+            "row": 6,
+            "col": 5,
+            "prop": 5,
+            "visualCol": 5
+        },
+        {
+            "row": 6,
+            "col": 6,
+            "prop": 6,
+            "visualCol": 6
+        }
+    ]
+}
+
+export {tableTernua};
