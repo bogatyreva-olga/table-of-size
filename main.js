6c276dce5d0e5504b90673fb6b27e0aaee5d4ab1
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-07-31 22:04:27 +0300

    remove modal element

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ let getSizeTable = (data) => {
 
     for (let i = 0; i < data.data.length; i++) {
         let tableRow = document.createElement("div");
-        if (i === 0) {
-            tableRow.classList.add("size-table__thead");
-        }
         tableRow.classList.add("size-table__tr");
 
         for (let k = 0; k < data.data[i].length; k++) {
@@ -23,41 +20,16 @@ let getSizeTable = (data) => {
     }
 }
 
-let getModalElement = () => {
-    return document.querySelector(".modal");
-}
-
-let showModalTableSize = () => {
-    let modalSizeTable = getModalElement();
-    let showBtnModalTableSize = document.querySelector(".show-size-table");
-    showBtnModalTableSize.addEventListener("click", () => {
-        modalSizeTable.style.display = "block";
-    })
-};
-
-let closeModalTableSize = () => {
-    let span = document.querySelector(".modal-close");
-    let modal = getModalElement();
-    span.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-    window.addEventListener("keydown", function (e) {
-        if (e.code === "Escape") {
-            modal.style.display = "none";
-        }
-    }, true);
-}
-
 let current = undefined;
 
 function showColorElement() {
+
     let colElements1 = document.querySelectorAll(".size-table__td");
 
     for (let i = 0; i < colElements1.length; i++) {
         colElements1[i].classList.remove("size-table__td-grey");
         colElements1[i].classList.remove("size-table__td-blue");
     }
-
     if (current) {
         current.classList.remove('size-table__td-blue');
     }
@@ -72,23 +44,21 @@ function showColorElement() {
 
     for (let i = 0; i < colElements.length; i++) {
         let currentCol = parseInt(colElements[i].dataset.col);
-        let currentRow = parseInt(rowElements[i].dataset.row)
-        if (currentCol > 0 && currentCol < numberCol) {
+        let currentRow = parseInt(rowElements[i].dataset.row);
+
+        if (currentCol > 0 && currentCol < numberCol && currentCol !== 0) {
             colElements[i].classList.add("size-table__td-grey");
             colElements[0].classList.add("size-table__td-blue");
         }
-        if(currentRow > 1 && currentRow < numberRow) {
+        if(currentRow > 0 && currentRow < numberRow) {
             rowElements[i].classList.add("size-table__td-grey");
-            rowElements[1].classList.add("size-table__td-blue");
+            rowElements[0].classList.add("size-table__td-blue");
         }
-
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     getSizeTable(tableTernua);
-    showModalTableSize();
-    closeModalTableSize();
 
     const items = document.querySelectorAll('.size-table__td')
     for (let i = 0; i < items.length; ++i) {
