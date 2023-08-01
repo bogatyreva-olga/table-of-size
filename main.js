2dcaacc02ad9b15b8bdb1c4923389aada68a76a9
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-08-01 15:32:26 +0300

    add media 768px

--- main.js
+++ main.js
@@ -23,36 +23,37 @@ let getSizeTable = (data) => {
 let current = undefined;
 
 function showColorElement() {
+    if (window.innerWidth > 768) {
+        let colElements1 = document.querySelectorAll(".size-table__td");
 
-    let colElements1 = document.querySelectorAll(".size-table__td");
-
-    for (let i = 0; i < colElements1.length; i++) {
-        colElements1[i].classList.remove("size-table__td-grey");
-        colElements1[i].classList.remove("size-table__td-blue");
-    }
-    if (current) {
-        current.classList.remove('size-table__td-blue');
-    }
-    this.classList.add('size-table__td-blue')
-    current = this;
-
-    let numberCol = parseInt(current.dataset.col);
-    let numberRow = parseInt(current.dataset.row);
-
-    let colElements = document.querySelectorAll('.size-table__td[data-row="' + numberRow + '"]');
-    let rowElements = document.querySelectorAll('.size-table__td[data-col="' + numberCol + '"]');
-
-    for (let i = 0; i < colElements.length; i++) {
-        let currentCol = parseInt(colElements[i].dataset.col);
-        let currentRow = parseInt(rowElements[i].dataset.row);
-
-        if (currentCol > 0 && currentCol < numberCol && currentCol !== 0) {
-            colElements[i].classList.add("size-table__td-grey");
-            colElements[0].classList.add("size-table__td-blue");
+        for (let i = 0; i < colElements1.length; i++) {
+            colElements1[i].classList.remove("size-table__td-grey");
+            colElements1[i].classList.remove("size-table__td-blue");
         }
-        if(currentRow > 0 && currentRow < numberRow) {
-            rowElements[i].classList.add("size-table__td-grey");
-            rowElements[0].classList.add("size-table__td-blue");
+        if (current) {
+            current.classList.remove('size-table__td-blue');
+        }
+        this.classList.add('size-table__td-blue')
+        current = this;
+
+        let numberCol = parseInt(current.dataset.col);
+        let numberRow = parseInt(current.dataset.row);
+
+        let colElements = document.querySelectorAll('.size-table__td[data-row="' + numberRow + '"]');
+        let rowElements = document.querySelectorAll('.size-table__td[data-col="' + numberCol + '"]');
+
+        for (let i = 0; i < colElements.length; i++) {
+            let currentCol = parseInt(colElements[i].dataset.col);
+            let currentRow = parseInt(rowElements[i].dataset.row);
+
+            if (currentCol > 0 && currentCol < numberCol && currentCol !== 0) {
+                colElements[i].classList.add("size-table__td-grey");
+                colElements[0].classList.add("size-table__td-blue");
+            }
+            if (currentRow > 0 && currentRow < numberRow) {
+                rowElements[i].classList.add("size-table__td-grey");
+                rowElements[0].classList.add("size-table__td-blue");
+            }
         }
     }
 }
